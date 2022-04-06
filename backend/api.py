e117c3b28de4be57074984102d078b7760d12936
mad201802 <mad201802@github.com>
2022-04-06 15:25:57 +0000

    add map method

--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,6 @@
 from map import PlaceMap, Colors
 from exceptions import OutOfMapException
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 
 MAP_SIZE = 100
@@ -9,26 +9,21 @@ MAP_NAME = "map_v1"
 app = FastAPI()
 map = PlaceMap(f"{MAP_NAME}_{MAP_SIZE}", MAP_SIZE)
 
-for i in range(MAP_SIZE):
-    map.place_pixel(i, i, Colors.RED)
-
 @app.get("/map")
-def get_image():
+async def get_image():
     return Response(map.get_image_as_byte_array(), media_type="image/png")
 
 @app.post("/place")
-def place_pixel(x: int, y: int, color: str):
+async def place_pixel(x: int, y: int, color: str):
     if x < 0 or x >= MAP_SIZE or y < 0 or y >= MAP_SIZE:
-        raise OutOfMapException(f"Position ({x+1},{y+1}) is outside the map")
-    
+        raise HTTPException(status_code=400, detail="Position is outside the map")
     try:
         map.place_pixel(x, y, Colors[color])
+        map.save_map()
+        return Response(status_code=200)
     except:
-        raise Exception()
-
-    map.save_map()
-    return Response(status_code=200)
+        raise HTTPException(status_code=400, detail="Invalid color")
 
 @app.get("/colors")
-def get_colors():
+async def get_colors():
     return {i.name: i.value for i in Colors}
--- backend/map.py
+++ backend/map.py
@@ -1,44 +1,80 @@
-from json import load
-from PIL import Image
-from enum import Enum
-from exceptions import OutOfMapException
-import io
 import os
+import pickle
+from enum import Enum
+from bitstring import BitArray
+from PIL import Image
+import numpy as np
 
-class Colors(Enum):
-    RED = "#FF0000"
-    GREEN = "#00FF00"
-    BLUE = "#0000FF"
+FILE_ENDING = ".map"
 
+class ColorMap(Enum):
+    WHITE = '0000'
+    BLACK = '1111'
 
-
-class PlaceMap():
-    def __init__(self, name: str, size: int):
+# MAP BYTE: 11 Bit Map Size, 4 Bit for color, x and y is equal to the offset in relation to mapsize
+class PlaceBitMap():
+    def __init__(self, name: str, p_map_size: int=100, n_map_bits: int = 11, n_color_bits: int=4):
+        if p_map_size > 2**n_map_bits:
+            raise ValueError("Map size is too big")
         self.name = name
-        self.size = size
-        self.image = self.load_image()
+        self.p_map_size = p_map_size
+        self.n_map_bits = n_map_bits
+        self.n_color_bits = n_color_bits
+        self.map_bits = self.create_or_load_map()
+    
+    def create_or_load_map(self):
+        if os.path.isfile(self.name + FILE_ENDING):
+            return self.load_map()
+        return self.create_map()
 
-    def load_image(self):
-        if os.path.isfile(f"{self.name}.png"):
-            return Image.open(f"{self.name}.png")
-        return Image.new('RGB', (self.size, self.size), (255, 255, 255))
+    def load_map(self):
+        return pickle.load(open(self.name + FILE_ENDING, "rb"))
 
-    def place_pixel(self, x: int, y: int, color: Colors):
-        if x < 0 or x >= self.size or y < 0 or y >= self.size:
-            raise OutOfMapException(f"Position ({x+1},{y+1}) is outside the map")
-        self.image.putpixel((x, y),  self.__hex_to_rgb(color.value[0]) + (255,))
+    def create_map(self):
+        map_bit_part = BitArray(uint=self.p_map_size, length=self.n_map_bits)
+        pixel_bit_part = BitArray(uint=0, length=self.p_map_size ** 2 * self.n_color_bits)
+        full_map = map_bit_part + pixel_bit_part
 
-    def save_map(self):
-        self.image.save(f"{self.name}.png")
+        # self.save_map(full_map)
+        return full_map
 
-    def get_image_as_byte_array(self):
-        img_byte_arr = io.BytesIO()
-        self.image.save(img_byte_arr, format='PNG')
-        return img_byte_arr.getvalue()
+    def get_map_size(self):
+        return f"{len(self.map_bits) / 8e+6} MByte"
 
-    def __hex_to_rgb(self, hex: str) -> tuple:
-        hex = hex.lstrip('#')
-        if len(hex) < 6: hex += "0"*(6-len(hex))
-        elif len(hex) > 6: hex = hex[0:6]
-        rgb = tuple(int(hex[i:i+2], 16) for i in (0,2,4))
-        return rgb
+    def render(self):
+        image = []
+        pixel_map = self.map_bits[self.n_map_bits:].bin
+        for row in range(0, self.p_map_size):
+            row_offset = row * self.p_map_size * self.n_color_bits
+            current_line = []
+            for col in range(0, self.p_map_size):
+                column_offset = col * self.n_color_bits
+                offset = row_offset + column_offset
+                color = ColorMap(pixel_map[offset: offset + self.n_color_bits])
+                if color is ColorMap.WHITE:
+                    current_line.append(0)
+                else:
+                    current_line.append(255)
+            image.append(current_line)
+        return np.array(image)
+
+    def set_pixel(self, x: int, y: int, color: ColorMap):
+        offset = self.n_map_bits + (y * (self.p_map_size - self.n_color_bits)) + (x * self.n_color_bits)
+        print(offset)
+        self.map_bits.overwrite(f'0b{color.value}', offset)
+
+    def save_map(self, object=None):
+        if object:
+            pickle.dump(object, open(self.name + FILE_ENDING, "wb"))
+        else:
+            pickle.dump(self.map_bits, open(self.name + FILE_ENDING, "wb"))
+
+map = PlaceBitMap(name="test")
+map.set_pixel(0, 0, ColorMap.BLACK)
+map.set_pixel(1, 1, ColorMap.BLACK)
+map.set_pixel(2, 2, ColorMap.BLACK)
+
+img_array = map.render()
+print(img_array.shape)
+img = Image.fromarray(img_array, 'L')
+img.save("img.png")
